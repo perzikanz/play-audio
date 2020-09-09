acc502f593dadfa3203924634babc77338511fe5
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-09-09 15:41:43 +0900

    コードをシンプルに書き換えた

---
+++ src/play.js
@@ -0,0 +1,11 @@
+const sounds = document.getElementsByClassName('sounds');
+
+for (let i = 0, l = sounds.length; l > i; i++) {
+  const file = sounds[i].getAttribute('data-file');
+  const src = `./src/audio/${file}.mp3`;
+  const audio = new Audio(src);
+
+  sounds[i].addEventListener('click', () => {
+    audio.play();
+  });
+}
