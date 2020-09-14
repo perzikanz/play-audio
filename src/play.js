273e381f50b0c55e19c5109e3e224b99143acf0d
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-09-14 20:00:03 +0900

    鍵盤をクリックすると音が鳴るように

--- src/play.js
+++ src/play.js
@@ -10,3 +10,16 @@ for (let i = 0, l = sounds.length; l > i; i++) {
     audio.play();
   });
 }
+
+const path = document.querySelectorAll('path');
+
+for (let i = 0, l = path.length; l > i; i++) {
+  const file = path[i].getAttribute('id');
+  const src = `./src/audio/${file}.mp3`;
+  const audio = new Audio(src);
+
+  path[i].addEventListener('click', () => {
+    audio.currentTime = 0;
+    audio.play();
+  });
+}
