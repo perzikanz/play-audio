72fa1496274a2c5a932aace8aae090891bdd3e8c
perzikanz <65535030+perzikanz@users.noreply.github.com>
2020-09-14 20:02:36 +0900

    挨拶ボタンを削除

--- src/play.js
+++ src/play.js
@@ -1,16 +1,3 @@
-const sounds = document.getElementsByClassName('sounds');
-
-for (let i = 0, l = sounds.length; l > i; i++) {
-  const file = sounds[i].getAttribute('data-file');
-  const src = `./src/audio/${file}.mp3`;
-  const audio = new Audio(src);
-
-  sounds[i].addEventListener('click', () => {
-    audio.currentTime = 0;
-    audio.play();
-  });
-}
-
 const path = document.querySelectorAll('path');
 
 for (let i = 0, l = path.length; l > i; i++) {
